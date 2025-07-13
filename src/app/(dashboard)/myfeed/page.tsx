2243f2eeccf1d08beb4f40b3f317fce7837ebf5e
Hussain_Sharif <99sharif00@gmail.com>
2025-07-13 21:38:01 +0530

    Updated Code for the my feed page

--- src/app/(dashboard)/myfeed/page.tsx
+++ src/app/(dashboard)/myfeed/page.tsx
@@ -7,23 +7,23 @@ import { useContent } from '../../../hooks/useContent';
 // import { testReduxStore } from '../libs/test-redux';
 
 export default function MyFeed() {
-  const { switchSection,  } = useContent();
+  const { switchSection,currentData,isLoading,hasError,errorMessage } = useContent();
 //   const { content, userPrefs, currentData,switchSection, isLoading, hasError, errorMessage } = useContent();
   
+  console.log(isLoading,hasError,errorMessage,'Current Data:', currentData);
+  // for(const category of currentData.news){
+  //   console.log(category.title)
+  // }
+
   // Test Redux on mount
   useEffect(() => {
      switchSection('personalized')
    }, [switchSection])
   return (
     <>
-      
-            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
             <h1>My Feed</h1>
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-            </div>
+            
         </div>
     </>
   );
